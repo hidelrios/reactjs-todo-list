dcfa5132c822d42da30992feb4a5b29bdb229250
hidelrios <hidels.rios@gmail.com>
2026-05-16 18:31:34 -0300

    feat: criado componente icon

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,12 +1,33 @@
-function App() {
-  return (
-    <div className="flex flex-col gap-2">
-      <Text variant="body-sm-bold" className="text-pink-base">Olá mundo!</Text>
-      <Text className="text-green-base">Olá mundo!</Text>
-      <Text variant="body-md-bold">Olá mundo!</Text>
-      <Text>Levar o dog pra passear</Text>
-    </div>
-  )
-}
+import Text from "./components/text";
+import Icon from "./components/icon";
 
-export default App
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+
+export default function App() {
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo!
+        </Text>
+        <Text className="text-green-base">Olá mundo!</Text>
+        <Text variant="body-md-bold">Olá mundo!</Text>
+        <Text>Levar o dog pra passear</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate={true} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={XIcon} />
+      </div>
+    </div>
+  );
+}
--- todo/vite.config.ts
+++ todo/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from "vite-plugin-svgr";
 
 
 // https://vite.dev/config/
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    svgr()
   ],
 })
