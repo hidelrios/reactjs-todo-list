423e712c037e8a785c3e2a18d022c00250b453ef
hidelrios <hidels.rios@gmail.com>
2026-05-16 18:38:11 -0300

    feat: criado componente badge

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -7,6 +7,7 @@ import XIcon from "./assets/icons/x.svg?react";
 import PencilIcon from "./assets/icons/pencil.svg?react";
 import PlusIcon from "./assets/icons/plus.svg?react";
 import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import Badge from "./components/badge";
 
 export default function App() {
   return (
@@ -27,6 +28,10 @@ export default function App() {
         <Icon svg={SpinnerIcon} animate={true} />
         <Icon svg={PencilIcon} />
         <Icon svg={XIcon} />
+      </div>
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
     </div>
   );
