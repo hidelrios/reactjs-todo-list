8b6598e1d4b4947534c9e0626dfbf2e0130c08e8
hidelrios <hidels.rios@gmail.com>
2026-05-16 15:47:46 -0300

    feat: criado componente text com cva

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
-
   return (
-    <>
-      <p className="text-pink-base">Todo App</p>
-    </>
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">Olá mundo!</Text>
+      <Text className="text-green-base">Olá mundo!</Text>
+      <Text variant="body-md-bold">Olá mundo!</Text>
+      <Text>Levar o dog pra passear</Text>
+    </div>
   )
 }
 
