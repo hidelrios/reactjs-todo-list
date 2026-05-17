bda605105e8ff8996ea45e793742774efc015320
hidelrios <hidels.rios@gmail.com>
2026-05-17 17:32:17 -0300

    feat: roteando pagina de componentes

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,48 +1,17 @@
-import Text from "./components/text";
-import Icon from "./components/icon";
-
-import TrashIcon from "./assets/icons/trash.svg?react";
-import CheckIcon from "./assets/icons/check.svg?react";
-import XIcon from "./assets/icons/x.svg?react";
-import PencilIcon from "./assets/icons/pencil.svg?react";
-import PlusIcon from "./assets/icons/plus.svg?react";
-import SpinnerIcon from "./assets/icons/spinner.svg?react";
-import Badge from "./components/badge";
-import Button from "./components/button";
-import ButtonIcon from "./components/button-icon";
+import { BrowserRouter, Routes, Route } from "react-router";
+import LayoutMain from "./pages/layout-main";
+import PageComponents from "./pages/page-components";
+import PageHome from "./pages/page-home";
 
 export default function App() {
   return (
-    <div className="grid gap-3">
-      <div className="flex flex-col gap-2">
-        <Text variant="body-sm-bold" className="text-pink-base">
-          Olá mundo!
-        </Text>
-        <Text className="text-green-base">Olá mundo!</Text>
-        <Text variant="body-md-bold">Olá mundo!</Text>
-        <Text>Levar o dog pra passear</Text>
-      </div>
-
-      <div className="flex gap-1">
-        <Icon svg={TrashIcon} className="fill-green-base" />
-        <Icon svg={CheckIcon} />
-        <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} animate={true} />
-        <Icon svg={PencilIcon} />
-        <Icon svg={XIcon} />
-      </div>
-      <div>
-        <Badge variant="secondary">5</Badge>
-        <Badge variant="primary">2 de 5</Badge>
-      </div>
-      <div>
-        <Button icon={PlusIcon}>Nova tarefa</Button>
-      </div>
-      <div className="flex gap-1">
-        <ButtonIcon icon={TrashIcon} disabled />
-        <ButtonIcon icon={TrashIcon} variant="secondary" />
-        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/componentes" element={<PageComponents />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
